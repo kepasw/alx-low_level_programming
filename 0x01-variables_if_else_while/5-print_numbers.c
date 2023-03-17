fd610da9dae0673ccf56c71a1b99983c347896c5
Whitney Kepas <whitney.kepas@gmail.com>
2023-03-17 23:15:35 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Programme that prints single
+ * digit number of base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '1'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
