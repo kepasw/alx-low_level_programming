6234e3fa4c17d5a85d67ce687a366e221f305e84
Whitney Kepas <whitney.kepas@gmail.com>
2023-03-22 17:16:30 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,9 +23,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
-	else 
+	else
 	{
-		printf("Last digit of %d is %d and less than 6 and not 0\n", n, x);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
 	return (0);
 }
